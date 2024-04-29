7c1c1d636f478a1c432c9fbe577588c1c877f694
Laszlo Nemeth <nemeth.rzr@gmail.com>
2024-04-29 09:22:01 +0200

    my solution

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,17 @@
 const rangeEl = document.querySelector('input[type="range"]');
 const inludeEl = document.querySelectorAll('input[type="checkbox"]');
 
-const includes = {
-  uppercase: false,
-  lowercase: false,
-  numbers: false,
-  symbols: false,
-};
+const btnGenerate = document.querySelector(".btn-generate");
+btnGenerate.addEventListener("click", handleBtnGenerate);
+const btnCopy = document.querySelector(".btn-copy");
+btnCopy.addEventListener("click", handleBtnCopy);
+
+const lengthEl = document.querySelector(".length");
+
+const resultIpt = document.getElementById("result_input");
+
+const alphabets = "ABCDEFGHIJKLMNOPQRSTUYVWZ";
+const symbols = "!()-.?[]_`~;:@#$%^&*+=";
 
 const strength = [
   ["", ""],
@@ -17,7 +22,6 @@ const strength = [
 ];
 
 rangeEl.addEventListener("change", function (event) {
-  const lengthEl = document.querySelector(".length");
   const val = event.target.value;
 
   lengthEl.innerText = val;
@@ -33,8 +37,6 @@ inludeEl.forEach((element) => {
     const indicatorEl = document.querySelector(".generator__indicators");
     indicatorEl.dataset.strength = strength[checkedEl.length][0];
 
-    const spanEl = document.createElement("span");
-
     indicatorEl.parentNode.children[1].innerText =
       strength[checkedEl.length][1];
 
@@ -43,3 +45,75 @@ inludeEl.forEach((element) => {
     console.log("checked" + event.target.name); */
   });
 });
+
+function handleBtnCopy(e) {
+  e.preventDefault();
+  navigator.clipboard.writeText(resultIpt.value);
+  btnCopy.querySelector("span").classList.add("show");
+}
+function handleBtnGenerate(e) {
+  e.preventDefault();
+  generate();
+  btnCopy.querySelector("span").classList.remove("show");
+}
+
+function getRandomAlphabet(uppercase = false) {
+  const randomIndex = Math.round(Math.random() * (alphabets.length - 1));
+
+  if (uppercase) return alphabets[randomIndex];
+  return alphabets[randomIndex].toLowerCase();
+}
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10);
+}
+function getRandomSymbol() {
+  const randomIndex = Math.round(Math.random() * (symbols.length - 1));
+
+  return symbols[randomIndex];
+}
+
+function generate() {
+  const includes = document.querySelectorAll('input[name="includes"]:checked');
+  let length = Number(lengthEl.innerText);
+  let types = [];
+  let result = "";
+  includes.forEach((element) => {
+    types.push(element.dataset.type);
+  });
+
+  const divide = Math.floor(length / types.length);
+  if (divide === 0 || divide === Infinity) {
+    return;
+  }
+
+  let firstCycle = true;
+
+  /** Első nekifutás */
+
+  /* utána random választunk a lehetőségek közül */
+
+  result.length != length;
+  while (result.length != length) {
+    const randomTypeIndex = Math.round(Math.random() * (types.length - 1));
+
+    switch (types[randomTypeIndex]) {
+      case "uppercase":
+        result += getRandomAlphabet(true);
+        break;
+      case "lowercase":
+        result += getRandomAlphabet(false);
+        break;
+      case "symbol":
+        result += getRandomSymbol();
+        break;
+      case "number":
+        result += getRandomNumber();
+        break;
+      default:
+        break;
+    }
+  }
+
+  console.log(result);
+  resultIpt.value = result;
+}
